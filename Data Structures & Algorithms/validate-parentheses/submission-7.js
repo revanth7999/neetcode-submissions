576662269712218aea975b1f90b561b159f6f640
Revanth Kumar <60778843+revanth7999@users.noreply.github.com>
2026-07-27 17:30:39 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let check = [];
+        
+        for(let par of s.split("")) {
+            if( par === '(' || 
+                par === '{' ||
+                par === '[' ) {
+                    check.push(par);
+                } else {
+                    let currentLast = check.length-1;
+                    if (currentLast < 0){
+                        return false;
+                    }
+                    if( (par === ')' && check[currentLast] === '(') ||
+                        (par === '}' && check[currentLast] === '{') ||
+                        (par === ']' && check[currentLast] === '[') ) {
+                            check.pop();
+                    } else {
+                        return false;
+                    }
+            }
+        
+        }
+        return check.length === 0;
+                
+    }
+}
